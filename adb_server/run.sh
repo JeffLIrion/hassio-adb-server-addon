5fbe13f8e697846d8a3e4cbe5c71043e179dda19
Jeff Irion <JeffLIrion@users.noreply.github.com>
2019-01-09 08:38:07 -0800

    Use 'adb start-server' instead of 'adb -a nodaemon server'

--- adb_server/run.sh
+++ adb_server/run.sh
@@ -6,7 +6,7 @@ CONFIG_PATH=/data/options.json
 DEVICES=$(jq --raw-output '.devices[]' $CONFIG_PATH)
 
 # start the ADB server
-adb -a nodaemon server
+adb start-server
 
 # connect to each of the devices
 for device in $DEVICES; do
