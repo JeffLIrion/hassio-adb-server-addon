f238d8fb74933cc9beb2ab96805a3a64c47fd3fc
Jeff Irion <jefflirion@users.noreply.github.com>
2019-01-20 14:04:26 -0800

    Copied changes from @NoedelVreter (with some modifications)

--- adb_server/run.sh
+++ adb_server/run.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
-set -e
 
-CONFIG_PATH=/data/options.json
+DEVICES=$(jq --raw-output '.devices[]' /data/options.json)
 
-DEVICES=$(jq --raw-output '.devices[]' $CONFIG_PATH)
+echo "Starting up ADB..."
 
-# start the ADB server
-adb start-server
+if [ -f /data/adbkey ]; then
+  echo "Found existing private key. Using it."
+  cp /data/adbkey /root/.android && cp /data/adbkey.pub /root/.android
+fi
 
-# connect to each of the devices
+while true; do
+  adb -a server nodaemon > /dev/null 2>&1
+  sleep 10
+done &
+
+echo "Server started. Waiting for 30 seconds..."
+sleep 30
+
+if [ ! -f /data/adbkey ]; then
+  echo "Saving newly generated private key."
+  cp /root/.android/adbkey /data/adbkey && cp /root/.android/adbkey.pub /data/adbkey.pub
+fi
+
+echo "Connecting to devices."
 for device in $DEVICES; do
-    adb connect $device
+   adb connect $device
 done
+echo "Done."
 
+while true; do
+  for device in $DEVICES; do
+    adb connect $device > /dev/null 2>&1
+  done
+  sleep 60
+done
